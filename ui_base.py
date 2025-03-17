1d9c56a1177f9f4222f5bd31f936d9510e7d3741
EchenDesktop <ericc@ERICDESKTOP-PC>
2025-03-17 17:07:32 -0600

    Json parsing perfected?

--- ui_base.py
+++ ui_base.py
@@ -13,21 +13,17 @@ root.title("Poke Scraper")
 searching_box = scrolledtext.ScrolledText(width=70, height=15, wrap=tk.WORD, font=("Arial", 8))
 searching_box.grid(row=0, column=0)
 
+stores_list = list(data.keys())
 
+for store in stores_list:
+    for product in data[store]["products"]:
+        searching_box.insert("1.0", store + ": " + product['custom_name'] + '\n')
+        searching_box.yview_moveto(0)
 
-bestbuy_products = data["Best Buy"]["products"]
-target_products = data["Target"]["products"]
-
-for product in bestbuy_products:
-    searching_box.insert("1.0", product['product_name'])
-    searching_box.yview_moveto(0)
 
 
 results_box = scrolledtext.ScrolledText(width=70, height=15, wrap=tk.WORD, font=("Arial", 12))
 results_box.grid(row=1, column=0)
 
-for product in bestbuy_products:
-    results_box.insert("1.0", product)  # Adds new results at the top
-    results_box.yview_moveto(0)
 
 root.mainloop()
