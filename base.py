99ac6164a4b3065784dc6c551c2bb50eca2e21cd
EchenDesktop <ericc@ERICDESKTOP-PC>
2025-03-17 18:27:59 -0600

    beautiful soup can't figure out target

--- base.py
+++ base.py
@@ -3,42 +3,50 @@ from bs4 import BeautifulSoup
 
 from twilio.rest import Client
 
-def check_BB():
+def run_check(url, referer):
     # Headers to mimic a real browser
     headers = {
         "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36",
-        "Referer": "https://www.bestbuy.com/",
+        "Referer": referer,
         "Accept-Language": "en-US,en;q=0.9",
     }
 
     # Make request
-    response = requests.get(bbUrl, headers=headers)
+    response = requests.get(url, headers=headers)
 
     # Check if request is blocked
     if response.status_code == 403:
-        return "Blocked by Best Buy!"
+        return "Request blocked by" + referer
     else:
         # Parse HTML
         soup = BeautifulSoup(response.content, "html.parser")
+        
+        if referer == "https://www.bestbuy.com/":
+            # Find the "Add to Cart" button
+            button = soup.find("button", {"data-button-state": True})
+            price_element = soup.find("div", class_="priceView-hero-price priceView-customer-price")
+            price = price_element.find("span").text if price_element else "[X.XX]"
 
-        # Find the "Add to Cart" button
-        button = soup.find("button", {"data-button-state": True})
-        price_element = soup.find("div", class_="priceView-hero-price priceView-customer-price")
-        price = price_element.find("span").text if price_element else "[X.XX]"
 
+            if button and "disabled" in button.attrs:
+                stock_status = "Item is NOT AVAILABLE."
+            else:
+                stock_status = "Item is IN STOCK for " + price + "!"
 
-        if button and "disabled" in button.attrs:
-            stock_status = "Item is NOT AVAILABLE."
-        else:
-            stock_status = "Item is IN STOCK for " + price + "!"
+        elif referer == "https://www.target.com/":
+            # Find the Add to Cart button
+            button = soup.find('button', class_= "styles_ndsBaseButton__W8Gl7 styles_md__X_r95 styles_mdGap__9J0yq styles_fullWidth__3XX6f styles_ndsButtonPrimary__tqtKH")
+            price_element = soup.find("span", {"data-test": "product-price"})
+            price = price_element.text.strip() if price_element else "Price not found"
+
+            if button:
+                is_disabled = "disabled" in button.attrs
+                print(f"✅ Button Found! Disabled: {is_disabled}")
+                stock_status = "Item is IN STOCK for " + price + "!"
+            else:
+                stock_status = "Item is NOT AVAILABLE."
 
     return stock_status
     
 
-def run_scraper():
-    schedule.every(5).seconds.do(check_BB)
-
-    while True:
-        schedule.run_pending()
-        time.sleep
 
--- ui_base.py
+++ ui_base.py
@@ -1,6 +1,9 @@
 import tkinter as tk
 from tkinter import messagebox, scrolledtext
 import json
+import base
+import schedule
+import time
 
 
 with open("links.json", "r", encoding="utf-8") as file:
@@ -10,20 +13,36 @@ root=tk.Tk()
 root.geometry("960x540")
 root.title("Poke Scraper")
 
-searching_box = scrolledtext.ScrolledText(width=70, height=15, wrap=tk.WORD, font=("Arial", 8))
+searching_box = scrolledtext.ScrolledText(width=70, height=15, wrap=tk.WORD, font=("Arial", 10))
 searching_box.grid(row=0, column=0)
 
 stores_list = list(data.keys())
 
 for store in stores_list:
     for product in data[store]["products"]:
-        searching_box.insert("1.0", store + ": " + product['custom_name'] + '\n')
+        searching_box.insert("1.0", store + ": " + product['custom_name'] +'\n')
         searching_box.yview_moveto(0)
 
+def check_links():
+    for store in stores_list:
+        for product in data[store]["products"]:
+            results_box.insert("1.0", store + ": " + product['custom_name'] + "[" + base.run_check(product["product_link"], data[store]["referer"]) + "]" + '\n')
+            results_box.yview_moveto(0)
 
-
-results_box = scrolledtext.ScrolledText(width=70, height=15, wrap=tk.WORD, font=("Arial", 12))
+results_box = scrolledtext.ScrolledText(width=70, height=15, wrap=tk.WORD, font=("Arial", 10))
 results_box.grid(row=1, column=0)
+
+run_script_btn = tk.Button(master=root, text="Run Script", command=check_links)
+run_script_btn.grid(row=0, column=1, pady=10)
+
+
+# def run_scraper():
+#     schedule.every(5).seconds.do(check_links)
+
+#     while True:
+#         schedule.run_pending()
+#         time.sleep
 
 
 root.mainloop()
+
